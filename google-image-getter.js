908ad379898057cb4b6ba8f9a015ce4d52fb94b7
joezcool02 <joseph.allen.1994@hotmail.com>
2017-06-16 15:04:52 +0100

    outline

---
+++ google-image-getter.js
@@ -0,0 +1,8 @@
+const GoogleImages = require('google-images');
+ 
+const client = new GoogleImages('011524445207737370194:dpe8mz5mdxa', 'AIzaSyCsXV4Qoq_vig3AJF63J8Rd_2hvgjFDa0E');
+
+client.search('Goat')
+	.then(function(images) {
+        console.log(images.url);
+	});
