85ce108385acf51aa25ecc697029773b2b3e680f
joezcool02 <joseph.allen.1994@hotmail.com>
2017-06-17 15:12:59 +0100

    Image Downloader

--- google-image-getter.js
+++ google-image-getter.js
@@ -1,8 +1,36 @@
 const GoogleImages = require('google-images');
+var fs = require('fs'),
+    request = require('request');
  
-const client = new GoogleImages('011524445207737370194:dpe8mz5mdxa', 'AIzaSyCsXV4Qoq_vig3AJF63J8Rd_2hvgjFDa0E');
+const client = new GoogleImages('011524445207737370194:dpe8mz5mdxa', 'AIzaSyAu_mnD5gzrjg6GltYb33EI7fySS29R6bY');
 
-client.search('Goat')
+let searchTerm = 'Goat';
+var imageUX;
+var imageUI;
+
+client.search(searchTerm)
 	.then(function(images) {
-        console.log(images);
+        imageUX = images[0].url;
+        console.log('Image UX : ',imageUX);
+
+        download(imageUX, 'images/UX.png', function(){
+            console.log('UX saved');
+        });
 	});
+    
+client.search(searchTerm, {page: 20})
+	.then(function(images) {
+        imageUI = images[0].url;
+        console.log('Image UI : ',imageUI);
+
+        download(imageUI, 'images/UI.png', function(){
+            console.log('UI saved');
+        });
+	});
+
+
+var download = function(uri, filename, callback){
+  request.head(uri, function(err, res, body){
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+};
